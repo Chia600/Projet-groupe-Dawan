f99be6a4bbfc3a862b7e189deb03937d29ef1c0d
Chia600 <benji300901@gmail.com>
2025-09-25 10:42:41 +0200

    spring initializr

---
+++ booktracker/src/test/java/com/dawanproject/booktracker/BooktrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawanproject.booktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooktrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
